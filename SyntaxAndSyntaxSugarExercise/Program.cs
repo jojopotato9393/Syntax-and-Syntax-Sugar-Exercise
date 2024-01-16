512bcd756fc9cdaa299f6a0d6e55d88f938bc235
Joseph Gaughan <jojopotato9393@gmail.com>
2024-01-16 13:31:58 -0600

    exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿using System.Reflection.Metadata;
+
+namespace SyntaxAndSyntaxSugarExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            var answer = 9;
+            var responce = "";
+            //  if (answer < 9)
+            // {
+            //     responce = $"{answer} is less then";
+            //  }
+            //  else
+            //  {
+            //      responce = $"{answer}is greater then ";
+            //  }
+            responce = (answer < 5) ? $"{answer} is less then"
+                    : $"{answer} is greater then or equal to five";
+            Console.WriteLine(responce);
+
+
+
+
+
+
+
+
+        }      
     }
 }
